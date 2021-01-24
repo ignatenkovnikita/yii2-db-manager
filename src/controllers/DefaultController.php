c1580d4558792a562d9a4463e7d2677397172f2c
Nikita Ignatenkov <ignatenkovnikita@users.noreply.github.com>
2021-01-24 03:05:21 +0300

    Update DefaultController.php
    add support preset

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -1,287 +1,250 @@
 <?php
 
-namespace bs\dbManager\controllers;
+namespace bs\dbManager\commands;
 
-use Yii;
-use yii\data\ArrayDataProvider;
-use yii\filters\VerbFilter;
-use yii\helpers\ArrayHelper;
-use yii\helpers\Html;
-use yii\helpers\StringHelper;
-use yii\web\Controller;
 use bs\dbManager\models\Dump;
 use bs\dbManager\models\Restore;
+use PDO;
+use PDOException;
 use Symfony\Component\Process\Process;
+use Yii;
+use yii\console\Controller;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Console;
+use yii\helpers\FileHelper;
+use yii\helpers\StringHelper;
 
 /**
- * Default controller.
+ * Database backup manager.
  */
-class DefaultController extends Controller
+class DumpController extends Controller
 {
+    public $db = 'db';
+    public $gzip = false;
+    public $storage = false;
+    public $file = null;
+    public $preset = null;
+
+    public $defaultAction = 'create';
+
+    public function options($actionID)
+    {
+        return [
+            'db',
+            'gzip',
+            'storage',
+            'file',
+            'preset',
+        ];
+    }
+
+    public function optionAliases()
+    {
+        return [
+            'db' => 'db',
+            'gz' => 'gzip',
+            's' => 'storage',
+            'f' => 'file',
+            'p' => 'preset'
+        ];
+    }
+
     /**
      * @return Module
      */
     public function getModule()
     {
-        return $this->module;
+        return Yii::$app->getModule('db-manager');
     }
 
-    /**
-     * @return array
-     */
-    public function behaviors()
+    private function deleteFiles()
     {
-        return [
-            'verbs' => [
-                'class' => VerbFilter::class,
-                'actions' => [
-                    'create' => ['post'],
-                    'delete' => ['post'],
-                    'delete-all' => ['post'],
-                    'restore' => ['get', 'post'],
-                    '*' => ['get'],
-                ],
-            ],
-        ];
+        $lastFiles = $this->getModule()->lastFiles;
+        $path = Yii::getAlias($this->getModule()->path);
+
+        $files = FileHelper::findFiles($path, ['only' => ['*.sql', '*.gz'], 'recursive' => FALSE]);
+
+        usort($files, function ($x, $y) {
+            return filemtime($x) > filemtime($y);
+        });
+
+        $allFiles = count($files);
+        if ($allFiles >= $lastFiles) {
+            foreach ($files as $file) {
+                if ($allFiles == $lastFiles) break;
+                FileHelper::unlink($file);
+                $allFiles--;
+            }
+        }
     }
 
     /**
-     * @inheritdoc
-     */
-    public function actionIndex()
-    {
-        $dataArray = $this->prepareFileData();
-        $dbList = $this->getModule()->dbList;
-        $model = new Dump($dbList, $this->getModule()->customDumpOptions);
-        $dataProvider = new ArrayDataProvider([
-            'allModels' => $dataArray,
-            'pagination' => [
-                'pageSize' => 30,
-            ],
-        ]);
-        $activePids = $this->checkActivePids();
-
-        return $this->render('index', [
-            'dataProvider' => $dataProvider,
-            'model' => $model,
-            'dbList' => $dbList,
-            'activePids' => $activePids,
-        ]);
-    }
-
-    /**
-     * @inheritdoc
+     * Create database dump.
      */
     public function actionCreate()
     {
+        $this->deleteFiles();
+
         $model = new Dump($this->getModule()->dbList, $this->getModule()->customDumpOptions);
-        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
-            $dbInfo = $this->getModule()->getDbInfo($model->db);
+        if (ArrayHelper::isIn($this->db, $this->getModule()->dbList)) {
+            $dbInfo = $this->getModule()->getDbInfo($this->db);
+            if ($this->preset) {
+                $model->preset = $this->preset;
+            }
             $dumpOptions = $model->makeDumpOptions();
+            if ($this->gzip) {
+                $dumpOptions['isArchive'] = true;
+            }
+
             $manager = $this->getModule()->createManager($dbInfo);
             $dumpPath = $manager->makePath($this->getModule()->path, $dbInfo, $dumpOptions);
             $dumpCommand = $manager->makeDumpCommand($dumpPath, $dbInfo, $dumpOptions);
             Yii::trace(compact('dumpCommand', 'dumpPath', 'dumpOptions'), get_called_class());
-            if ($model->runInBackground) {
-                $this->runProcessAsync($dumpCommand);
-            } else {
-                $this->runProcess($dumpCommand);
-            }
-        } else {
-            Yii::$app->session->setFlash('error', Yii::t('dbManager', 'Dump request invalid.') . '<br>' . Html::errorSummary($model));
-        }
+            $process = new Process($dumpCommand, null, null, null, 60 * 30);
+            $process->setTimeout($this->getModule()->timeout);
+            $process->run();
+            if ($process->isSuccessful()) {
+                $uploadResult = true;
+                if ($this->storage) {
+                    if (Yii::$app->has('backupStorage')) {
+                        Console::output('Opening: ' . $dumpPath);
 
-        return $this->redirect(['index']);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function actionDownload($id)
-    {
-        $dumpPath = $this->getModule()->path . StringHelper::basename(ArrayHelper::getValue($this->getModule()->getFileList(), $id));
-
-        return Yii::$app->response->sendFile($dumpPath);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function actionRestore($id)
-    {
-        $dumpFile = $this->getModule()->path . StringHelper::basename(ArrayHelper::getValue($this->getModule()->getFileList(), $id));
-        $model = new Restore($this->getModule()->dbList, $this->getModule()->customRestoreOptions);
-        if (Yii::$app->request->isPost) {
-            if ($model->load(Yii::$app->request->post()) && $model->validate()) {
-                $dbInfo = $this->getModule()->getDbInfo($model->db);
-                $restoreOptions = $model->makeRestoreOptions();
-                $manager = $this->getModule()->createManager($dbInfo);
-                $restoreCommand = $manager->makeRestoreCommand($dumpFile, $dbInfo, $restoreOptions);
-                Yii::trace(compact('restoreCommand', 'dumpFile', 'restoreOptions'), get_called_class());
-                if ($model->runInBackground) {
-                    $this->runProcessAsync($restoreCommand, true);
-                } else {
-                    $this->runProcess($restoreCommand, true);
+                        $storage = Yii::createObject([
+                            'class' => 'creocoder\flysystem\LocalFilesystem',
+                            'path' => dirname($dumpPath),
+                        ]);
+                        $uploadResult = Yii::$app->backupStorage->writeStream(StringHelper::basename($dumpPath), $storage->readStream(StringHelper::basename($dumpPath)));
+                    } else {
+                        Console::output('Storage component is not configured.');
+                    }
                 }
-
-                return $this->redirect(['index']);
-            }
-        }
-
-        return $this->render('restore', [
-            'model' => $model,
-            'file' => $dumpFile,
-            'id' => $id,
-        ]);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function actionStorage($id)
-    {
-        if (Yii::$app->has('backupStorage')) {
-            $dumpname = StringHelper::basename(ArrayHelper::getValue($this->getModule()->getFileList(), $id));
-            $dumpPath = $this->getModule()->path . $dumpname;
-            $exists = Yii::$app->backupStorage->has($dumpname);
-            if ($exists) {
-                Yii::$app->backupStorage->delete($dumpname);
-                Yii::$app->session->setFlash('success', Yii::t('dbManager', 'Dump deleted from storage.'));
+                if ($uploadResult !== false) {
+                    Console::output('Dump successfully created.');
+                }
             } else {
-                $stream = fopen($dumpPath, 'r+');
-                Yii::$app->backupStorage->writeStream($dumpname, $stream);
-                Yii::$app->session->setFlash('success', Yii::t('dbManager', 'Dump uploaded to storage.'));
+                //Console::output(print_r($process->getErrorOutput(), 1));
+                Console::output('Dump failed create.');
+            }
+        } else {
+            Console::output('Database configuration not found.');
+        }
+    }
+
+    /**
+     * Restore database dump.
+     */
+    public function actionRestore()
+    {
+        $model = new Restore($this->getModule()->dbList);
+        if (is_null($this->file)) {
+            if ($this->storage) {
+                if (Yii::$app->has('backupStorage')) {
+                    foreach (Yii::$app->backupStorage->listContents() as $file) {
+                        $fileList[] = [
+                            'basename' => $file['basename'],
+                            'timestamp' => $file['timestamp'],
+                        ];
+                    }
+                } else {
+                    Console::output('Storage component is not configured.');
+                }
+            } else {
+                foreach ($this->getModule()->getFileList() as $file) {
+                    $fileList[] = [
+                        'basename' => StringHelper::basename($file),
+                        'timestamp' => filectime($file),
+                    ];
+                }
+            }
+            ArrayHelper::multisort($fileList, ['timestamp'], [SORT_DESC]);
+            $this->file = ArrayHelper::getValue(array_shift($fileList), 'basename');
+        }
+        $runtime = null;
+        $dumpFile = null;
+        if ($this->storage) {
+            if (Yii::$app->has('backupStorage')) {
+                if (Yii::$app->backupStorage->has($this->file)) {
+                    $runtime = Yii::getAlias('@runtime/backups');
+                    if (!is_dir($runtime)) {
+                        FileHelper::createDirectory($runtime);
+                    }
+                    $dumpFile = $runtime . '/' . $this->file;
+                    file_put_contents($dumpFile, Yii::$app->backupStorage->read($this->file));
+                } else {
+                    Console::output('File not found.');
+                }
+            } else {
+                Console::output('Storage component is not configured.');
+            }
+        } else {
+            $fileExists = $this->getModule()->path . $this->file;
+            if (file_exists($fileExists)) {
+                $dumpFile = $fileExists;
+            } else {
+                Console::output('File not found.');
             }
         }
-
-        return $this->redirect(['index']);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function actionDelete($id)
-    {
-        $dumpFile = $this->getModule()->path . StringHelper::basename(ArrayHelper::getValue($this->getModule()->getFileList(), $id));
-        if (unlink($dumpFile)) {
-            Yii::$app->session->setFlash('success', Yii::t('dbManager', 'Dump deleted successfully.'));
+        if (ArrayHelper::isIn($this->db, $this->getModule()->dbList)) {
+            $dbInfo = $this->getModule()->getDbInfo($this->db);
+            $restoreOptions = $model->makeRestoreOptions();
+            $manager = $this->getModule()->createManager($dbInfo);
+            $restoreCommand = $manager->makeRestoreCommand($dumpFile, $dbInfo, $restoreOptions);
+            Yii::trace(compact('restoreCommand', 'dumpFile', 'restoreOptions'), get_called_class());
+            $process = new Process($restoreCommand);
+            $process->setTimeout($this->getModule()->timeout);
+            $process->run();
+            if (!is_null($runtime)) {
+                FileHelper::removeDirectory($runtime);
+            }
+            if ($process->isSuccessful()) {
+                Console::output('Dump successfully restored.');
+            } else {
+                Console::output('Dump failed restored.');
+            }
         } else {
-            Yii::$app->session->setFlash('error', Yii::t('dbManager', 'Error deleting dump.'));
+            Console::output('Database configuration not found.');
         }
-
-        return $this->redirect(['index']);
     }
 
     /**
-     * @inheritdoc
+     * Deleting all dumps.
      */
     public function actionDeleteAll()
     {
-        if (!empty($this->getModule()->getFileList())) {
-            $fail = [];
-            foreach ($this->getModule()->getFileList() as $file) {
-                if (!unlink($file)) {
-                    $fail[] = $file;
+        Console::output('Do you want to delete all dumps? [yes|no]');
+        $answer = trim(fgets(STDIN));
+        if (!strncasecmp($answer, 'y', 1)) {
+            if (!empty($this->getModule()->getFileList())) {
+                $fail = [];
+                foreach ($this->getModule()->getFileList() as $file) {
+                    if (!unlink($file)) {
+                        $fail[] = $file;
+                    }
                 }
-            }
-            if (empty($fail)) {
-                Yii::$app->session->setFlash('success', Yii::t('dbManager', 'All dumps successfully removed.'));
-            } else {
-                Yii::$app->session->setFlash('error', Yii::t('dbManager', 'Error deleting dumps.'));
-            }
-        }
-
-        return $this->redirect(['index']);
-    }
-
-    /**
-     * @param $command
-     * @param bool $isRestore
-     */
-    protected function runProcess($command, $isRestore = false)
-    {
-        $process = new Process($command);
-        $process->setTimeout($this->getModule()->timeout);
-        $process->run();
-        if ($process->isSuccessful()) {
-            $msg = (!$isRestore) ? Yii::t('dbManager', 'Dump successfully created.') : Yii::t('dbManager', 'Dump successfully restored.');
-            Yii::$app->session->addFlash('success', $msg);
-        } else {
-            $msg = (!$isRestore) ? Yii::t('dbManager', 'Dump failed.') : Yii::t('dbManager', 'Restore failed.');
-            Yii::$app->session->addFlash('error', $msg . '<br>' . 'Command - ' . $command . '<br>' . $process->getOutput() . $process->getErrorOutput());
-            Yii::error($msg . PHP_EOL . 'Command - ' . $command . PHP_EOL . $process->getOutput() . PHP_EOL . $process->getErrorOutput());
-        }
-    }
-
-    /**
-     * @param $command
-     * @param bool $isRestore
-     */
-    protected function runProcessAsync($command, $isRestore = false)
-    {
-        $process = new Process($command);
-        $process->setTimeout($this->getModule()->timeout);
-        $process->start();
-        $pid = $process->getPid();
-        $activePids = Yii::$app->session->get('backupPids', []);
-        if (!$process->isRunning()) {
-            if ($process->isSuccessful()) {
-                $msg = (!$isRestore) ? Yii::t('dbManager', 'Dump successfully created.') : Yii::t('dbManager', 'Dump successfully restored.');
-                Yii::$app->session->addFlash('success', $msg);
-            } else {
-                $msg = (!$isRestore) ? Yii::t('dbManager', 'Dump failed.') : Yii::t('dbManager', 'Restore failed.');
-                Yii::$app->session->addFlash('error', $msg . '<br>' . 'Command - ' . $command . '<br>' . $process->getOutput() . $process->getErrorOutput());
-                Yii::error($msg . PHP_EOL . 'Command - ' . $command . PHP_EOL . $process->getOutput() . PHP_EOL . $process->getErrorOutput());
-            }
-        } else {
-            $activePids[$pid] = $command;
-            Yii::$app->session->set('backupPids', $activePids);
-            Yii::$app->session->addFlash('info', Yii::t('dbManager', 'Process running with pid={pid}', ['pid' => $pid]) . '<br>' . $command);
-        }
-    }
-
-    /**
-     * @return array
-     */
-    protected function checkActivePids()
-    {
-        $activePids = Yii::$app->session->get('backupPids', []);
-        $newActivePids = [];
-        if (!empty($activePids)) {
-            foreach ($activePids as $pid => $cmd) {
-                $process = new Process('ps -p ' . $pid);
-                $process->setTimeout($this->getModule()->timeout);
-                $process->run();
-                if (!$process->isSuccessful()) {
-                    Yii::$app->session->addFlash('success',
-                        Yii::t('dbManager', 'Process complete!') . '<br> PID=' . $pid . ' ' . $cmd);
+                if (empty($fail)) {
+                    Console::output('All dumps successfully removed.');
                 } else {
-                    $newActivePids[$pid] = $cmd;
+                    Console::output('Error deleting dumps.');
                 }
             }
         }
-        Yii::$app->session->set('backupPids', $newActivePids);
-
-        return $newActivePids;
     }
 
     /**
-     * @return array
+     * Test connection to database.
      */
-    protected function prepareFileData()
+    public function actionTestConnection()
     {
-        $dataArray = [];
-        foreach ($this->getModule()->getFileList() as $id => $file) {
-            $dataArray[] = [
-                'id' => $id,
-                'type' => pathinfo($file, PATHINFO_EXTENSION),
-                'name' => StringHelper::basename($file),
-                'size' => Yii::$app->formatter->asSize(filesize($file)),
-                'create_at' => Yii::$app->formatter->asDatetime(filectime($file)),
-            ];
+        if (ArrayHelper::isIn($this->db, $this->getModule()->dbList)) {
+            $info = $this->getModule()->getDbInfo($this->db);
+            try {
+                new PDO($info['dsn'], $info['username'], $info['password']);
+                Console::output('Connection success.');
+            } catch (PDOException $e) {
+                Console::output('Connection failed: ' . $e->getMessage());
+            }
+        } else {
+            Console::output('Database configuration not found.');
         }
-        ArrayHelper::multisort($dataArray, ['create_at'], [SORT_DESC]);
-
-        return $dataArray ?: [];
     }
 }
